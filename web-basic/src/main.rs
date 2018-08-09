8e11a729abfffa08bde0b235ad04845853a727f6
zhangjunfang <zhangjunfang0505@163.com>
2018-08-09 11:20:44 +0800

    2018-8-9

--- web-basic/src/main.rs
+++ web-basic/src/main.rs
@@ -83,15 +83,16 @@ fn main() {
    // let mut v=Vec::with_capacity(16usize );
     server::new(|| { //使用应用工厂创建新的http服务器
         vec![
-             App::with_state(AppState{counter: Cell::new(0)})
-                 //new()
+             App::
+                 //with_state(AppState{counter: Cell::new(0)})
+                 new() //多状态应用情况之一
                  .prefix("/mm")  //添加单一应用的统一前缀
                  //限制访问  必须是post提交  否则找不到资源
                  .filter(pred::Method(http::Method::GET)) //前置过滤器
                  .middleware(middleware::Logger::default()) // 日志中间件
-                 .resource("hh",|r|{r.f(|_:&HttpRequest<AppState>|{"hello  visitor !!!!"})})
+                 .resource("hh",|r|{r.f(|_:&HttpRequest|{"hello  visitor !!!!"})})
                  .boxed(),//多状态 必须使用这个方法
-             App::with_state(AppState{counter: Cell::new(0)})
+             App::with_state(AppState{counter: Cell::new(0)})//多状态应用情况之一
                  //new()
                  .prefix("/user")  //添加单一应用的统一前缀
                  .middleware(middleware::Logger::default())// 日志中间件
@@ -106,7 +107,7 @@ fn main() {
                  //.resource("/json",|r|{ r.f(json)})//路由空字符对应方法处理handler为index函数
                  .resource("/session",|r|{ r.f(session)})//路由字符创/session对应方法处理handler为session函数);
                  .boxed(),//多状态 必须使用这个方法
-             App::with_state(AppState{counter: Cell::new(0)}) // <- create app with state
+             App::with_state(AppState{counter: Cell::new(0)}) //多状态应用情况之一
                  // enable logger
                  .prefix("/state")  //添加单一应用的统一前缀
                  .middleware(middleware::Logger::default())
@@ -114,7 +115,7 @@ fn main() {
                  .resource("/", |r| r.f(state))
                  .boxed(),//多状态 必须使用这个方法
 
-             App::with_state(AppState2{counter: Cell::new(0)}) // <- create app with state
+             App::with_state(AppState2{counter: Cell::new(0)}) //多状态应用情况之一
                  .prefix("/json")  //添加单一应用的统一前缀
                  .middleware(middleware::Logger::default())// enable logger
                  .resource("/", |r| r.method(http::Method::GET).f(json))
